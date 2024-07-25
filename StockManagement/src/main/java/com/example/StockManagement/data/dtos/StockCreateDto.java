a5723e6fc07d2158f45c4a05d95c345cbe0849f0
valdetad <valdeta.demiri@hotmail.com>
2024-07-25 12:20:59 +0100

    Stock Dto added

--- StockManagement/src/main/java/com/example/StockManagement/data/dtos/StockCreateDto.java
+++ StockManagement/src/main/java/com/example/StockManagement/data/dtos/StockCreateDto.java
@@ -1,4 +1,13 @@
 package com.example.StockManagement.data.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class StockCreateDto {
+    private Integer quantity;
 }
