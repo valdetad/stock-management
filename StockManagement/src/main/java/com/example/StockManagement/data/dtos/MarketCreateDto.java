69defb774c0dac242385ba66ac3769ced02f6210
valdetad <valdeta.demiri@hotmail.com>
2024-07-25 12:19:44 +0100

    Market Dto added

--- StockManagement/src/main/java/com/example/StockManagement/data/dtos/MarketCreateDto.java
+++ StockManagement/src/main/java/com/example/StockManagement/data/dtos/MarketCreateDto.java
@@ -1,4 +1,14 @@
 package com.example.StockManagement.data.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class MarketCreateDto {
+    private String name;
+    private String location;
 }
