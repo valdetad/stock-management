36bb29b8d389f4fec14e9f6b1d3f9cd7c6ef3032
valdetad <valdeta.demiri@hotmail.com>
2024-07-25 14:21:59 +0100

    .

--- StockManagement/src/main/java/com/example/StockManagement/controller/ProductController.java
+++ StockManagement/src/main/java/com/example/StockManagement/controller/ProductController.java
@@ -3,14 +3,11 @@ package com.example.StockManagement.controller;
 import com.example.StockManagement.data.model.Product;
 import com.example.StockManagement.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -22,6 +19,5 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.findAll();
-
     }
 }
