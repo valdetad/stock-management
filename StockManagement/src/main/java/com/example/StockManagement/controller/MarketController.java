699ccf76483452b2a863e1f3b446a74bb8460a0d
valdetad <valdeta.demiri@hotmail.com>
2024-08-05 09:53:39 +0100

    .

--- StockManagement/src/main/java/com/example/StockManagement/controller/MarketController.java
+++ StockManagement/src/main/java/com/example/StockManagement/controller/MarketController.java
@@ -1,9 +1,7 @@
 package com.example.StockManagement.controller;
 
 import com.example.StockManagement.data.model.Market;
-import com.example.StockManagement.data.model.Product;
 import com.example.StockManagement.service.MarketService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
