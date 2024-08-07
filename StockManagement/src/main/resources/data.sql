c96713d578b4839f229032a453d0226c2a766805
valdetad <valdeta.demiri@hotmail.com>
2024-08-07 10:29:37 +0100

    .

--- StockManagement/src/main/resources/data.sql
+++ StockManagement/src/main/resources/data.sql
@@ -1,12 +1,13 @@
 INSERT INTO product
-(name,
- category,
- price,
- description,
- barcode)
+    (name,
+     category,
+     price,
+     description,
+     barcode)
 
 VALUES ('Example Product',
         'Category1',
         19.99,
         'This is an example product description.',
         '1234567890123');
+
