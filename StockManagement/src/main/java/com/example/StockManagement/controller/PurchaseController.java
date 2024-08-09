affb581cc432e30c9cd7bf60882f7a776b5674e9
valdetad <valdeta.demiri@hotmail.com>
2024-08-09 09:55:34 +0100

    .

--- StockManagement/src/main/java/com/example/StockManagement/controller/PurchaseController.java
+++ StockManagement/src/main/java/com/example/StockManagement/controller/PurchaseController.java
@@ -1,13 +1,17 @@
 package com.example.StockManagement.controller;
 
-import com.example.StockManagement.data.model.Purchase;
 import com.example.StockManagement.service.PurchaseService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.itextpdf.text.DocumentException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/purchase")
@@ -16,8 +20,17 @@ public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
 
-    @GetMapping
-    public List<Purchase> getAllPurchases() {
-        return purchaseService.findAll();
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_Purchase_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        purchaseService.export(response);
     }
 }
--- StockManagement/src/main/java/com/example/StockManagement/service/PurchaseService.java
+++ StockManagement/src/main/java/com/example/StockManagement/service/PurchaseService.java
@@ -1,22 +1,39 @@
 package com.example.StockManagement.service;
 
-import com.example.StockManagement.data.model.Product;
 import com.example.StockManagement.data.model.Purchase;
-import com.example.StockManagement.repository.ProductRepository;
 import com.example.StockManagement.repository.PurchaseRepository;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class PurchaseService {
+
     @Autowired
     private PurchaseRepository purchaseRepository;
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+
+        PdfWriter.getInstance(document, response.getOutputStream());
+        document.open();
+
+        Paragraph paragraph = new Paragraph("This is a title.");
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+
+        document.add(paragraph);
+        document.close();
+    }
 
     public List<Purchase> findAll() {
         return purchaseRepository.findAll();
     }
-
 }
-
