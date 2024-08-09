12a75c6a6f7c26496cea03dbb5bf4b96a9f6b566
valdetad <valdeta.demiri@hotmail.com>
2024-08-09 10:42:56 +0100

    .

--- StockManagement/src/main/java/com/example/StockManagement/service/PurchaseService.java
+++ StockManagement/src/main/java/com/example/StockManagement/service/PurchaseService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -21,15 +24,30 @@ public class PurchaseService {
         Document document = new Document(PageSize.A4);
 
         PdfWriter.getInstance(document, response.getOutputStream());
+
         document.open();
 
-        Paragraph paragraph = new Paragraph("This is a title.");
+        // Format the current date
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDate = dateFormatter.format(new Date());
+
+        // Create a title with the current date
+        String title = "Purchase Date " + currentDate;
+        Paragraph titleParagraph = new Paragraph(title);
+        titleParagraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(titleParagraph);
+
+        // Add additional content
+        Paragraph paragraph = new Paragraph("This is the body of the document.");
         paragraph.setAlignment(Paragraph.ALIGN_CENTER);
 
         Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
         fontParagraph.setSize(12);
+        paragraph.setFont(fontParagraph);
 
         document.add(paragraph);
+
         document.close();
     }
 
